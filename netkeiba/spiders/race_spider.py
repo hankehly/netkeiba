600e666425ba4d2679fe5682bbebe0364ba9df39
Hank Ehly <henry.ehly@gmail.com>
2018-10-08 15:12:30 +0900

    Add some scraping logic

--- netkeiba/spiders/race_spider.py
+++ netkeiba/spiders/race_spider.py
@@ -10,13 +10,14 @@ class RaceSpiderSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow=r'pid=race_top&date=[0-9]+')),
-        Rule(LinkExtractor(allow=r'\/race\/list\/[0-9]+\/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'\/race\/list\/[0-9]+\/')),
+        Rule(LinkExtractor(allow=r'\/race\/[0-9]+'), callback='parse_item'),
     )
 
     def parse_item(self, response):
-        print(response)
+        # print(response)
         i = {}
-        # i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        # i['name'] = response.xpath('//div[@id="name"]').extract()
-        # i['description'] = response.xpath('//div[@id="description"]').extract()
+        # i['horses'] = response.css('table[summary="レース結果"] a[id^="umalink_"]::text').extract()
+        i['raw_rows'] = response.css('table[summary="レース結果"] tr').extract()
+        print(i)
         return i
