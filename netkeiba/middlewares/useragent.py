77b2a37b4f33ded9a0b521619b272f3e9dec5363
Hank Ehly <henry.ehly@gmail.com>
2018-10-22 22:59:28 +0900

    Obtain crawler-user-agents via npm

--- netkeiba/middlewares/useragent.py
+++ netkeiba/middlewares/useragent.py
@@ -7,8 +7,10 @@ from itertools import chain
 class UserAgentMiddleware(object):
     def __init__(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        crawler_list = json.load(open(f'{dir_path}/../resources/crawler-user-agents.json', 'r'))
-        self.user_agent_list = list(chain.from_iterable([item['instances'] for item in crawler_list]))
+        fp = open(f'{dir_path}/../../node_modules/crawler-user-agents/crawler-user-agents.json', 'r')
+        crawler_user_agents = json.load(fp)
+        instances = [item['instances'] for item in crawler_user_agents]
+        self.user_agent_list = list(chain.from_iterable(instances))
 
     def process_request(self, request, spider):
         user_agent = random.choice(self.user_agent_list)
