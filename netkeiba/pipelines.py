1a6ee4099d5898e8ba4879ec701b0c1f61b4ee1c
Hank Ehly <henry.ehly@gmail.com>
2018-10-23 09:27:10 +0900

    Fix pipeline causing all items to be dropped

--- netkeiba/pipelines.py
+++ netkeiba/pipelines.py
@@ -7,9 +7,9 @@ class RacePipeline(object):
             item['horse']['total_wins'] = 0
 
         order_of_finish = item['race_finisher'].get('order_of_finish')
-        finish_time = item['race_finisher'].get('finish_time')
+        finish_time_seconds = item['race_finisher'].get('finish_time_seconds')
 
-        if order_of_finish != 'disqualified' and finish_time is None:
-            raise DropItem(f'Missing race_finisher.finish_time in {item}')
+        if order_of_finish != 'disqualified' and finish_time_seconds is None:
+            raise DropItem(f'Missing race_finisher.finish_time_seconds in {item}')
 
         return item
