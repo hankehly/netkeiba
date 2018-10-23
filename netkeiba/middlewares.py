f18b2049168be8b60a69b9bfc0c2230d11108b7f
Hank Ehly <henry.ehly@gmail.com>
2018-10-23 09:43:40 +0900

    Set log file name dynamically

--- netkeiba/middlewares.py
+++ netkeiba/middlewares.py
@@ -3,14 +3,15 @@ import os
 import random
 from itertools import chain
 
+from netkeiba.settings import PROJECT_ROOT
+
 
 class UserAgentMiddleware(object):
     def __init__(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        fp = open(f'{dir_path}/../node_modules/crawler-user-agents/crawler-user-agents.json', 'r')
-        crawler_user_agents = json.load(fp)
-        instances = [item['instances'] for item in crawler_user_agents]
-        self.user_agent_list = list(chain.from_iterable(instances))
+        file_path = os.path.join(PROJECT_ROOT, 'node_modules', 'crawler-user-agents', 'crawler-user-agents.json')
+        crawler_user_agents = json.load(open(file_path, 'r'))
+        multi_dim_list = [item['instances'] for item in crawler_user_agents]
+        self.user_agent_list = list(chain.from_iterable(multi_dim_list))
 
     def process_request(self, request, spider):
         user_agent = random.choice(self.user_agent_list)
--- netkeiba/settings.py
+++ netkeiba/settings.py
@@ -1,3 +1,8 @@
+import os
+from datetime import datetime
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 BOT_NAME = 'netkeiba'
 
 SPIDER_MODULES = ['netkeiba.spiders']
@@ -18,3 +23,5 @@ DOWNLOAD_DELAY = 4.0
 DOWNLOADER_MIDDLEWARES = {
     'netkeiba.middlewares.UserAgentMiddleware': 300,
 }
+
+LOG_FILE = os.path.join(PROJECT_ROOT, 'logs', datetime.now().isoformat(timespec='seconds'))
