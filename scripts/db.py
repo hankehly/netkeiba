66c0584f067c32d4a1967beb118aa36243fb6364
Hank Ehly <henry.ehly@gmail.com>
2018-11-17 15:07:26 +0900

    Create sqlite database in root dir

--- scripts/db.py
+++ scripts/db.py
@@ -5,11 +5,11 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 def main():
-    conn = sqlite3.connect(os.path.join(PROJECT_ROOT, 'utils', 'netkeiba.sqlite'))
+    conn = sqlite3.connect(os.path.join(PROJECT_ROOT, 'netkeiba.sqlite'))
     c = conn.cursor()
-    with open(os.path.join(PROJECT_ROOT, 'utils', 'create_tables.sql'), 'r') as f:
+    with open(os.path.join(PROJECT_ROOT, 'utils', 'setup.sql'), 'r') as f:
         sql = f.read()
-        c.executescript()
+        c.executescript(sql)
 
 
 if __name__ == '__main__':
