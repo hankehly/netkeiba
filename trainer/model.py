0824f0424092345b2b2bc6b1780430c11a13ce34
Hank Ehly <henry.ehly@gmail.com>
2018-12-01 06:44:30 +0900

    Add notes to model.py

--- trainer/model.py
+++ trainer/model.py
@@ -137,25 +137,74 @@ def prediction_test():
         't_career_earnings',
     ]
 
+    # upcoming race lists are posted on pages like the following
+    # https://race.netkeiba.com/?pid=race_old&id=c201809050103
+    # you should be able to take a URL as input and get all the data you need via parsing..
+    # because inputting it by hand takes forever.
+
     # http://db.netkeiba.com/race/201808050703/
+    r_racetrack = 'kyoto'
+    r_course_type = 'turf'
+    r_weather = 'sunny'
+    r_distance = 1600
+    r_date = '2018-11-24'
+    r_dirt_condition = ''
+    r_turf_condition = 'good'
+    r_impost_category = 'age_based'
+    r_is_non_winner_regional_horse_allowed = 1
+    r_is_winner_regional_horse_allowed = 0
+    r_is_regional_jockey_allowed = 0
+    r_is_foreign_horse_allowed = 0
+    r_is_foreign_horse_and_trainer_allowed = 0
+    r_is_apprentice_jockey_allowed = 0
+    r_is_female_only = 0
+
+    # dynamically calculated attrs (but needed from user upon prediction)
     contender_count = 11
 
     X_test_raw = pd.DataFrame([
         [  # 1st horse
-                      # c_id (SKIP)
-            55,       # c_weight_carried
-            6,        # c_post_position
-                      # c_order_of_finish (SKIP)
-                      # c_order_of_finish_lowered (SKIP)
-                      # c_finish_time (SKIP)
-            478,      # c_horse_weight
-            -4,       # c_horse_weight_diff
-            1,        # c_popularity
-            2.7,      # c_first_place_odds
-                      # r_id (SKIP)
-                      # r_key (SKIP)
-            'kyoto',  # r_racetrack
+            55,            # c_weight_carried
+            6,             # c_post_position
+            478,           # c_horse_weight
+            -4,            # c_horse_weight_diff
+            1,             # c_popularity
+            2.7,           # c_first_place_odds
+            2,             # h_total_races
+            1,             # h_total_wins
+            'male',        # h_sex
+            '2016-02-10',  # h_birthday
+            2.5,           # h_user_rating
+
+            915,           # j_career_1st_place_count
+            696,           # j_career_2nd_place_count
+            540,           # j_career_3rd_place_count
+            2864,          # j_career_4th_place_or_below_count
+            2742,          # j_career_turf_race_count
+            556,           # j_career_turf_win_count
+            2273,          # j_career_dirt_race_count
+            359,           # j_career_dirt_win_count
+            .182,          # j_career_1st_place_rate
+            .321,          # j_career_1st_2nd_place_rate
+            .429,          # j_career_any_place_rate
+            2146768.,      # j_career_earnings
+
+            409,           # t_career_1st_place_count
+            386,           # t_career_2nd_place_count
+            406,           # t_career_3rd_place_count
+            3635,          # t_career_4th_place_or_below_count
+            2795,          # t_career_turf_race_count
+            247,           # t_career_turf_win_count
+            1958,          # t_career_dirt_race_count
+            153,           # t_career_dirt_win_count
+            .085,          # t_career_1st_place_rate
+            .164,          # t_career_1st_2nd_place_rate
+            .248,          # t_career_any_place_rate
+            854946.4,      # t_career_earnings
         ]
     ], columns=columns)
+
+    # race details
+    # details of each horse/jockey/trainer pair
 
     predict(X_test_raw)
