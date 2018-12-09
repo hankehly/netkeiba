14ba048c33ab4a0c52295edfdc79242590e419fc
Hank Ehly <henry.ehly@gmail.com>
2018-12-10 07:31:08 +0900

    Move prediction and export logic

--- trainer/task.py
+++ trainer/task.py
@@ -1,8 +1,13 @@
 import os
 import sys
+from datetime import datetime
 
 import numpy as np
+from sklearn.externals import joblib
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import StratifiedShuffleSplit
+
+from trainer.pipeline import full_pipeline
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, PROJECT_ROOT)
@@ -29,5 +34,27 @@ for train_idx, test_idx in sss.split(X, y):
     X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
     y_train, y_test = y[train_idx], y[test_idx]
 
-# change this line to train different models
-train_rand_forest_reg_random_search(X_train, X_test, y_train, y_test)
+X_train_prep = full_pipeline.fit_transform(X_train)
+model = train_rand_forest_reg_random_search(X_train, y_train)
+
+predictions = model.predict(X_train_prep)
+
+rmse = np.sqrt(mean_squared_error(y_train, predictions))
+mae = mean_absolute_error(y_train, predictions)
+
+timestamp = datetime.now().isoformat(timespec='minutes').replace(':', '')
+dirname = f'rand_forest_reg_{timestamp}'
+os.mkdir(dirname)
+
+joblib.dump(model, os.path.join(dirname, 'model.joblib'))
+joblib.dump(X_train, os.path.join(dirname, 'X_train.joblib'))
+joblib.dump(y_train, os.path.join(dirname, 'y_train.joblib'))
+joblib.dump(X_test, os.path.join(dirname, 'X_test.joblib'))
+joblib.dump(y_test, os.path.join(dirname, 'y_test.joblib'))
+
+with open(os.path.join(dirname, 'results.txt'), 'w') as f:
+    f.write(f'# {dirname}\n')
+    f.write(f'RMSE: {rmse}\n')
+    f.write(f'MAE: {mae}\n')
+
+print(f'Exported to {dirname}')
