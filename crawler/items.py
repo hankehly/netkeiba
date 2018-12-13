baf82d3b45f9e4dc3e281e2dc479db5e13ffea56
Hank Ehly <henry.ehly@gmail.com>
2018-12-13 09:03:58 +0900

    Delete unused items

--- crawler/items.py
+++ crawler/items.py
@@ -8,38 +8,3 @@ class WebPageItem(scrapy.Item):
     def __repr__(self):
         html = f"{self['html'][:40]}... (truncated)" if len(self['html']) > 55 else self['html']
         return repr({'url': self['url'], 'html': html})
-
-
-class PageItem(scrapy.Item):
-    key = scrapy.Field()
-    html = scrapy.Field()
-    url = scrapy.Field()
-    type = scrapy.Field()
-
-
-class Race(PageItem):
-    """
-    Ex. http://db.netkeiba.com/race/201806050103/
-    """
-    pass
-
-
-class Horse(PageItem):
-    """
-    Ex. http://db.netkeiba.com/horse/2016105200/
-    """
-    pass
-
-
-class TrainerResult(PageItem):
-    """
-    Ex. http://db.netkeiba.com/trainer/result/01059/
-    """
-    pass
-
-
-class JockeyResult(PageItem):
-    """
-    Ex. http://db.netkeiba.com/jockey/result/05509/
-    """
-    pass
