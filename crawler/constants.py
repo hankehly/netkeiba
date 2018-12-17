f13b50d56ff7984239d69e12fdba0943a65a6f66
Hank Ehly <henry.ehly@gmail.com>
2018-12-18 08:30:55 +0900

    Extract constants to separate file

---
+++ crawler/constants.py
@@ -0,0 +1,38 @@
+RACETRACKS = {
+    '札幌': 'sapporo',
+    '函館': 'hakodate',
+    '福島': 'fuma',
+    '新潟': 'niigata',
+    '東京': 'tokyo',
+    '中山': 'nakayama',
+    '中京': 'chukyo',
+    '京都': 'kyoto',
+    '阪神': 'hanshin',
+    '小倉': 'ogura'
+}
+
+WEATHER = {
+    '曇': 'cloudy',
+    '晴': 'sunny',
+    '雨': 'rainy',
+    '雪': 'snowy',
+}
+
+IMPOST_CATEGORIES = {
+    '馬齢': 'age_based',
+    '定量': 'age_sex_based',
+    '別定': 'decided_per_race',
+    'ハンデ': 'handicap'
+}
+
+COURSE_TYPES = {
+    '芝': 'turf',
+    'ダ': 'dirt',
+    '障': 'obstacle'
+}
+
+HORSE_SEX = {
+    '牝': 'female',
+    '牡': 'male',
+    'セ': 'castrated'
+}
--- crawler/spiders/race.py
+++ crawler/spiders/race.py
@@ -4,48 +4,10 @@ from datetime import datetime
 import scrapy
 from bs4 import BeautifulSoup
 
+from constants import RACETRACKS, COURSE_TYPES, IMPOST_CATEGORIES, HORSE_SEX
 from crawler.parsers.horse import HorseParser
 from crawler.parsers.jockey_result import JockeyResultParser
 from crawler.parsers.trainer_result import TrainerResultParser
-
-RACETRACKS = {
-    '札幌': 'sapporo',
-    '函館': 'hakodate',
-    '福島': 'fuma',
-    '新潟': 'niigata',
-    '東京': 'tokyo',
-    '中山': 'nakayama',
-    '中京': 'chukyo',
-    '京都': 'kyoto',
-    '阪神': 'hanshin',
-    '小倉': 'ogura'
-}
-
-WEATHER = {
-    '曇': 'cloudy',
-    '晴': 'sunny',
-    '雨': 'rainy',
-    '雪': 'snowy',
-}
-
-IMPOST_CATEGORIES = {
-    '馬齢': 'age_based',
-    '定量': 'age_sex_based',
-    '別定': 'decided_per_race',
-    'ハンデ': 'handicap'
-}
-
-COURSE_TYPES = {
-    '芝': 'turf',
-    'ダ': 'dirt',
-    '障': 'obstacle'
-}
-
-HORSE_SEX = {
-    '牝': 'female',
-    '牡': 'male',
-    'セ': 'castrated'
-}
 
 
 class RaceSpider(scrapy.Spider):
