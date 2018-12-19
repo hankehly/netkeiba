464f58c53c2c2c4e5e8a88b644950758a5d26040
Hank Ehly <henry.ehly@gmail.com>
2018-12-19 09:04:29 +0900

    Add stub cron task

--- server/cron.py
+++ server/cron.py
@@ -2,7 +2,16 @@ from datetime import date, timedelta
 
 from django.core.management import call_command
 
+from server.models import WebPage
+
 
 def scrape():
     min_date = (date.today() - timedelta(weeks=1)).strftime('%Y-%m-%d')
     call_command('scrape', min_date=min_date)
+
+
+def import_scraped_content():
+    for page in WebPage.objects.all():
+        data = page.parse()
+        # depending on the type of page
+        # it may contain data for one or multiple models
