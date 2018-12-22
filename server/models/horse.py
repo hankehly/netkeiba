21781e94fdedc5b5a1cc7409a384db5096f9d534
Hank Ehly <henry.ehly@gmail.com>
2018-12-22 21:50:32 +0900

    Allow horse records to be created with only key

--- server/models/horse.py
+++ server/models/horse.py
@@ -5,10 +5,10 @@ from server.models.base import BaseModel
 
 class Horse(BaseModel):
     key = models.CharField(max_length=255)
-    total_races = models.PositiveSmallIntegerField()
-    total_wins = models.PositiveSmallIntegerField()
-    birthday = models.DateField()
-    sex = models.ForeignKey('HorseSex', on_delete=models.CASCADE)
+    total_races = models.PositiveSmallIntegerField(null=True)
+    total_wins = models.PositiveSmallIntegerField(null=True)
+    birthday = models.DateField(null=True)
+    sex = models.ForeignKey('HorseSex', on_delete=models.CASCADE, null=True)
     user_rating = models.FloatField(blank=True, null=True)
 
     class Meta:
