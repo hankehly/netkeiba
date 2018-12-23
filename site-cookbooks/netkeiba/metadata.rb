953dc2a6e7f7f87bf70bf0c01374960f31c3488c
Hank Ehly <henry.ehly@gmail.com>
2018-12-23 10:10:03 -0600

    Generate netkeiba cookbook

---
+++ site-cookbooks/netkeiba/metadata.rb
@@ -0,0 +1,20 @@
+name 'netkeiba'
+maintainer 'The Authors'
+maintainer_email 'henry.ehly@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures netkeiba'
+long_description 'Installs/Configures netkeiba'
+version '0.1.0'
+chef_version '>= 13.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/netkeiba/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/netkeiba'
---
+++ site-cookbooks/netkeiba/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: netkeiba
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
