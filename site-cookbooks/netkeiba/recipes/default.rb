a84a3edbff2985992cf22f7c2dbc1dee4deb5de4
Hank Ehly <henry.ehly@gmail.com>
2018-12-24 12:48:46 -0600

    Add td_agent match block

--- site-cookbooks/netkeiba/recipes/default.rb
+++ site-cookbooks/netkeiba/recipes/default.rb
@@ -27,3 +27,30 @@ pyenv_global python_version
 pyenv_plugin 'virtualenv' do
 	git_url 'https://github.com/pyenv/pyenv-virtualenv'
 end
+
+pyenv_pip 'awscli'
+
+directory '/var/log/fluentd' do
+  	owner 'td-agent'
+  	group 'td-agent'
+  	mode '0755'
+  	action :create
+end
+
+aws_access_key_id = data_bag_item('aws', 'credentials')['aws_access_key_id']
+aws_secret_access_key = data_bag_item('aws', 'credentials')['aws_secret_access_key']
+
+td_agent_match 'netkeiba' do
+  	type 's3'
+  	tag 'netkeiba.**'
+  	action :create
+  	parameters({
+  		aws_key_id: aws_access_key_id,
+  		aws_sec_key: aws_secret_access_key,
+  		s3_bucket: 'octo-waffle',
+  		path: 'netkeiba/log/',
+  		buffer_path: '/var/log/fluentd/buffer/td',
+  		time_slice_format: '%Y%m%d%H'
+  	})
+end
+
