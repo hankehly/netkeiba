405323d732f22067ec1559786b9428b33870d772
Hank Ehly <henry.ehly@gmail.com>
2018-12-27 13:05:20 -0600

    Remove unused log dir

--- netkeiba/settings.py
+++ netkeiba/settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-LOG_DIR = os.path.join(BASE_DIR, 'log')
 TMP_DIR = os.path.join(BASE_DIR, 'tmp')
 
 # Quick-start development settings - unsuitable for production
