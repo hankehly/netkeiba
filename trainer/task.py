7c1e2daceaa49f951a997f9151a7b98366893bb1
Hank Ehly <henry.ehly@gmail.com>
2018-12-31 10:22:42 -0600

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,17 +1,14 @@
 import os
 import sys
-from datetime import datetime
 
 import numpy as np
-from sklearn.externals import joblib
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import StratifiedShuffleSplit
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, PROJECT_ROOT)
 from trainer.pipeline import full_pipeline
 from trainer.model import train_rand_forest_reg_random_search
-from src.utils import read_netkeiba
+from trainer.util import read_netkeiba, upload_model
 
 np.random.seed(42)
 
@@ -25,29 +22,4 @@ for train_idx, test_idx in sss.split(X, y):
 
 X_train_prep = full_pipeline.fit_transform(X_train)
 model = train_rand_forest_reg_random_search(X_train_prep, y_train)
-model_filename = 'model.joblib'
-joblib.dump(model, os.path.join(model_filename))
-
-timestamp = datetime.now().isoformat(timespec='minutes').replace(':', '')
-bucket_name = os.environ.get('GCLOUD_BUCKET')
-gcs_model_path = os.path.join('gs://', bucket_name, timestamp, model_filename)
-os.subprocess.check_call(['gsutil', 'cp', model_filename, gcs_model_path], stderr=sys.stdout)
-
-# predictions = model.predict(X_train_prep)
-# rmse = np.sqrt(mean_squared_error(y_train, predictions))
-# mae = mean_absolute_error(y_train, predictions)
-#
-# dirname = os.path.join(PROJECT_ROOT, 'tmp', 'models', f'rand_forest_reg_{timestamp}')
-# os.mkdir(dirname)
-#
-# joblib.dump(X_train, os.path.join(dirname, 'X_train.joblib'))
-# joblib.dump(y_train, os.path.join(dirname, 'y_train.joblib'))
-# joblib.dump(X_test, os.path.join(dirname, 'X_test.joblib'))
-# joblib.dump(y_test, os.path.join(dirname, 'y_test.joblib'))
-#
-# with open(os.path.join(dirname, 'results.txt'), 'w') as f:
-#     f.write(f'# {dirname}\n')
-#     f.write(f'RMSE: {rmse}\n')
-#     f.write(f'MAE: {mae}\n')
-#
-# print(f'Exported to {dirname}')
+upload_model(model)
