93f7fb048ff379cf35bf5a451aa131f2b970a555
Hank Ehly <henry.ehly@gmail.com>
2019-01-01 13:09:39 -0600

    Fix os.environ usage

--- lambda/function.py
+++ lambda/function.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
     bucket_name = os.environ.get('S3_BUCKET')
-    s3_path = os.environ('EC2_SSH_KEY_S3_PATH')
+    s3_path = os.environ.get('EC2_SSH_KEY_S3_PATH')
     pkey_path = '/tmp/pkey.pem'
     s3.Object(bucket_name, s3_path).download_file(pkey_path)
     pkey = paramiko.RSAKey.from_private_key_file(pkey_path)
