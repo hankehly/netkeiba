ada15494f2a5b4c48c3b8001b8098b7a6b8b2608
Hank Ehly <henry.ehly@gmail.com>
2019-01-02 07:26:48 -0600

    Add min_date support and more substantial logging to pipeline command

--- server/management/commands/pipeline.py
+++ server/management/commands/pipeline.py
@@ -1,9 +1,14 @@
+import logging
 from datetime import datetime, timedelta
 from subprocess import call
 
 import pytz
 from django.conf import settings
 from django.core.management import BaseCommand, call_command
+
+from server.argtype import date_string
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -12,14 +17,29 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-s', '--shutdown', action='store_true', default=False,
                             help='Execute shutdown command after pipeline completion')
+        parser.add_argument('-m', '--min-date', dest='min_date', type=date_string,
+                            help='Process all data from this date to present (fmt: YYYY-MM-DD)')
 
     def handle(self, *args, **options):
-        one_week_ago = (datetime.now(tz=pytz.timezone(settings.TIME_ZONE)) - timedelta(weeks=1)).strftime('%Y-%m-%d')
+        start_dt = datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
+        logger.info(f'started pipeline command at {start_dt}')
 
-        call_command('scrape', min_date=one_week_ago)
-        call_command('import', min_date=one_week_ago)
+        if options['min_date']:
+            logger.info(f"user specified minimum date is {options['min_date']}")
+            min_date = datetime.strptime(options['min_date'], '%Y-%m-%d')
+        else:
+            logger.info('user specified minimum date does not exist (defaulting to now - 1 week)')
+            min_date = (start_dt - timedelta(weeks=1)).strftime('%Y-%m-%d')
+
+        call_command('scrape', min_date=min_date)
+        call_command('import', min_date=min_date)
         call_command('backup')
         call_command('submit')
 
+        finish_dt = datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
+        duration = (finish_dt - start_dt).seconds
+        logger.info(f'Finished pipeline command at {finish_dt} ({duration} seconds)')
+
         if options['shutdown']:
+            logger.info('Received shutdown option. Shutting down now.')
             call('sudo shutdown -h now', shell=True)
