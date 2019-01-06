7992337bb267099b1b4d710ac338ac7bcac662cf
Hank Ehly <henry.ehly@gmail.com>
2019-01-05 19:40:15 -0600

    Refactor weather spider

--- crawler/spiders/weather.py
+++ crawler/spiders/weather.py
@@ -3,6 +3,13 @@ import scrapy
 from server.models import Race
 
 
+def _get_racetrack_block_no(name: str) -> str:
+    """
+    TODO: Implement
+    """
+    return '1236'
+
+
 class WeatherSpider(scrapy.Spider):
     """
     https://www.data.jma.go.jp/obd/stats/etrn/view/10min_a1.php?prec_no=45&block_no=1236&year=2018&month=12&day=2
@@ -14,19 +21,10 @@ class WeatherSpider(scrapy.Spider):
         urls = []
         base_url = 'https://www.data.jma.go.jp/obd/stats/etrn/view/10min_a1.php?prec_no=45&'
         for race in Race.objects.all():
-            block_no = self._get_racetrack_block_no(race.racetrack.name)
-            year = race.date.year
-            month = race.date.month
-            day = race.date.day
-            url = f'{base_url}block_no={block_no}&year={year}&month={month}&day={day}'
+            block_no = _get_racetrack_block_no(race.racetrack.name)
+            url = f'{base_url}block_no={block_no}&year={race.date.year}&month={race.date.month}&day={race.date.day}'
             urls.append(url)
         return urls
 
     def parse(self, response):
         pass
-
-    def _get_racetrack_block_no(self, name: str) -> str:
-        """
-        TODO: Implement
-        """
-        return '1236'
