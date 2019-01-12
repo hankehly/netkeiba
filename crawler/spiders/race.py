93248e45ac386038d51c9b1084e8de12b2aee22d
Hank Ehly <henry.ehly@gmail.com>
2019-01-12 12:31:59 +0900

    Update race crawler

--- crawler/spiders/race.py
+++ crawler/spiders/race.py
@@ -11,6 +11,43 @@ from crawler.parsers.jockey_result import JockeyResultParser
 from crawler.parsers.trainer_result import TrainerResultParser
 
 
+class TABLE_COL:
+    POST_POS = 0
+    HORSE = 3
+    AGE_SEX = 4
+    WEIGHT_CARRIED = 5
+    JOCKEY = 6
+    TRAINER = 7
+    WEIGHT = 8
+    ODDS = 9
+    POPULARITY = 10
+
+
+def _parse_post_position(row):
+    return row.select('td')[TABLE_COL.POST_POS].string
+
+
+def _parse_horse_weight(row):
+    weight_col = row.select('td')[TABLE_COL.WEIGHT].string
+    weight_search = re.search('([0-9]+)\(([+-]?[0-9]+)\)', weight_col)
+    return int(weight_search.group(1)) if weight_search else None
+
+
+def _parse_horse_weight_diff(row):
+    weight_col = row.select('td')[TABLE_COL.WEIGHT].string
+    weight_search = re.search('([0-9]+)\(([+-]?[0-9]+)\)', weight_col)
+    return int(weight_search.group(2)) if weight_search else None
+
+
+def _parse_horse_url(row):
+    return row.select('td')[TABLE_COL.HORSE].select_one('a').get('href')
+
+
+def _parse_horse_key(row):
+    h_url = _parse_horse_url(row)
+    return re.search('/horse/([0-9]+)', h_url).group(1)
+
+
 def _prefix_keys(obj: dict, prefix: str):
     acc = {}
     for key, value in obj.items():
@@ -19,11 +56,66 @@ def _prefix_keys(obj: dict, prefix: str):
     return acc
 
 
+def _parse_horse_sex(row):
+    horse_sex_symbol = re.search('[^0-9]', row.select('td')[TABLE_COL.AGE_SEX].text).group()
+    return HORSE_SEX.get(horse_sex_symbol)
+
+
+def _parse_weight_carried(row):
+    return float(row.select('td')[TABLE_COL.WEIGHT_CARRIED].text)
+
+
+def _parse_jockey_key(row):
+    jockey_column = row.select('td')[TABLE_COL.JOCKEY]
+    if jockey_column.select_one('a[href]'):
+        j_url = jockey_column.select_one('a').get('href')
+        j_key = re.search('/jockey/([0-9]+)', j_url).group(1)
+    else:
+        j_key = ''
+    return j_key
+
+
+def _parse_trainer_key(row):
+    t_url = row.select('td')[TABLE_COL.TRAINER].select_one('a').get('href')
+    return re.search('/trainer/([0-9]+)', t_url).group(1)
+
+
+def _parse_odds(row):
+    return float(row.select('td')[TABLE_COL.ODDS].text)
+
+
+def _parse_popularity(row):
+    return int(row.select('td')[TABLE_COL.POPULARITY].text)
+
+
+def _parse_previous_order_of_finish(row, date):
+    h_key = _parse_horse_key(row)
+    with connection.cursor() as cursor:
+        previous_order_of_finish_query = '''
+            SELECT c.order_of_finish
+            FROM race_contenders c
+                   LEFT JOIN races r ON c.race_id = r.id
+            WHERE c.horse_id = (
+              SELECT id
+              FROM horses
+              WHERE key = %s
+              LIMIT 1
+            )
+              AND r.date < %s
+            ORDER BY r.date DESC
+            LIMIT 1
+        '''
+        result = cursor.execute(previous_order_of_finish_query, [h_key, date]).fetchone()
+        return result[0] if result else None
+
+
 class RaceSpider(scrapy.Spider):
     """
     scrapy crawl race -a race_url='https://race.netkeiba.com/?pid=race_old&id=xxx' -o xxx.jl
     """
+
     name = 'race'
+
     allowed_domains = ['race.netkeiba.com', 'db.netkeiba.com']
 
     def __init__(self, *args, **kwargs):
@@ -74,44 +166,22 @@ class RaceSpider(scrapy.Spider):
         }
 
         for row in contenders:
-            h_url = row.select('td')[1].select_one('a').get('href')
-            h_key = re.search('/horse/([0-9]+)', h_url).group(1)
-            horse_sex_symbol = re.search('[^0-9]', row.select('td')[2].text).group()
-            h_sex = HORSE_SEX.get(horse_sex_symbol)
-            c_weight_carried = float(row.select('td')[3].text)
-
-            jockey_column = row.select('td')[4]
-            if jockey_column.select_one('a[href]'):
-                j_url = jockey_column.select_one('a').get('href')
-                j_key = re.search('/jockey/([0-9]+)', j_url).group(1)
-            else:
-                j_key = ''
-
-            t_url = row.select('td')[5].select_one('a').get('href')
-            t_key = re.search('/trainer/([0-9]+)', t_url).group(1)
-            c_first_place_odds = float(row.select('td')[6].text)
-            c_popularity = int(row.select('td')[7].text)
-
-            with connection.cursor() as cursor:
-                previous_order_of_finish_query = '''
-                    SELECT c.order_of_finish
-                    FROM race_contenders c
-                           LEFT JOIN races r ON c.race_id = r.id
-                    WHERE c.horse_id = (
-                      SELECT id
-                      FROM horses
-                      WHERE key = %s
-                      LIMIT 1
-                    )
-                      AND r.date < %s
-                    ORDER BY r.date DESC
-                    LIMIT 1
-                '''
-                result = cursor.execute(previous_order_of_finish_query, [h_key, race['date']]).fetchone()
-                c_previous_order_of_finish = result[0] if result else None
+            c_post_position = _parse_post_position(row)
+            h_url = _parse_horse_url(row)
+            h_key = _parse_horse_key(row)
+            h_sex = _parse_horse_sex(row)
+            j_key = _parse_jockey_key(row)
+            t_key = _parse_trainer_key(row)
+            c_first_place_odds = _parse_odds(row)
+            c_popularity = _parse_popularity(row)
+            c_weight_carried = _parse_weight_carried(row)
+            c_previous_order_of_finish = _parse_previous_order_of_finish(row, race['date'])
+            c_horse_weight = _parse_horse_weight(row)
+            c_horse_weight_diff = _parse_horse_weight_diff(row)
 
             meta = {
                 'data': {
+                    'c_post_position': c_post_position,
                     'h_key': h_key,
                     'h_sex': h_sex,
                     'j_key': j_key,
@@ -120,6 +190,8 @@ class RaceSpider(scrapy.Spider):
                     'c_popularity': c_popularity,
                     'c_weight_carried': c_weight_carried,
                     'c_previous_order_of_finish': c_previous_order_of_finish,
+                    'c_horse_weight': c_horse_weight,
+                    'c_horse_weight_diff': c_horse_weight_diff,
                 }
             }
 
