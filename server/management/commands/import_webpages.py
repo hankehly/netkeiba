103bb5bcb801bb59aa702c5759664b01e8982a0e
Hank Ehly <henry.ehly@gmail.com>
2019-01-29 21:55:55 +0900

    Update webpage import script

--- server/management/commands/import_webpages.py
+++ server/management/commands/import_webpages.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pytz
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.db import IntegrityError
 
 from server.models import WebPage
 
@@ -20,6 +21,8 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('csv_path', help='The absolute path to the CSV resource containing exported scrapy items')
+        parser.add_argument('--chunk-size', type=int, default=1000,
+                            help='The chunk size to use when parsing a large CSV file (default 1000)')
 
     def handle(self, *args, **options):
         tzinfo = pytz.timezone(settings.TIME_ZONE)
@@ -31,13 +34,22 @@ class Command(BaseCommand):
         self.logger.debug(f'Loading csv data from {csv_path}')
 
         i = 1
-        for df in pd.read_csv(csv_path, chunksize=1000):
+        for df in pd.read_csv(csv_path, chunksize=options['chunk_size']):
             self.logger.info(f'processing chunk {i}')
             for j, item in df.iterrows():
-                self.logger.debug(f'chunk: {i}, item: {j} <url: {item.url}, fingerprint: {item.fingerprint}>')
                 crawled_at = dateutil.parser.isoparse(item.crawled_at).replace(tzinfo=tzinfo)
-                defaults = {'html': item.html, 'url': item.url, 'crawled_at': crawled_at}
-                WebPage.objects.update_or_create(fingerprint=item.fingerprint, defaults=defaults)
+                defaults = {'fingerprint': item.fingerprint, 'html': item.html, 'url': item.url, 'crawled_at': crawled_at}
+                try:
+                    WebPage.objects.create(**defaults)
+                except IntegrityError:
+                    self.logger.debug(f'chunk: {i}, item: {j} already exists <url: {item.url}, fingerprint: {item.fingerprint}>')
+                    page = WebPage.objects.get(fingerprint=item.fingerprint)
+                    page.url = item.url
+                    page.html = item.html
+                    page.crawled_at = crawled_at
+                    page.save()
+                else:
+                    self.logger.debug(f'chunk: {i}, item: {j} <url: {item.url}, fingerprint: {item.fingerprint}>')
             i += 1
 
         end_time = datetime.now(tzinfo)
