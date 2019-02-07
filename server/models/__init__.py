06b9c02bc64535d57a9aabfad474a56bc126bdb5
Hank Ehly <henry.ehly@gmail.com>
2019-02-07 21:54:45 +0900

    Add BettingTicket model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,4 +1,5 @@
 from server.models.base import BaseModel
+from server.models.betting_ticket import BettingTicket
 from server.models.horse import Horse
 from server.models.jockey import Jockey
 from server.models.owner import Owner
