78bd92b6409ad37e1e68fbcb3d79ca3c5d25c1ab
Hank Ehly <henry.ehly@gmail.com>
2019-02-16 11:06:21 +0900

    Format settings file

--- netkeiba/settings.py
+++ netkeiba/settings.py
@@ -1,44 +1,22 @@
 import os
 
-from django.apps import apps
 from django.conf import settings
 
-NAMESPACE = apps.get_app_config('netkeiba').name.upper()
+LOG_DIR = getattr(settings, 'NETKEIBA_LOG_DIR', os.path.join(settings.BASE_DIR, 'log'))
 
-LOG_DIR = getattr(
-    settings,
-    f'{NAMESPACE}_LOG_DIR',
-    os.path.join(settings.BASE_DIR, 'log')
-)
+TMP_DIR = getattr(settings, 'NETKEIBA_TMP_DIR', os.path.join(settings.BASE_DIR, 'tmp'))
 
-TMP_DIR = getattr(
-    settings,
-    f'{NAMESPACE}_TMP_DIR',
-    os.path.join(settings.BASE_DIR, 'tmp')
-)
+TIME_ZONE = getattr(settings, 'NETKEIBA_TIME_ZONE', 'UTC')
 
-TIME_ZONE = getattr(
-    settings,
-    f'{NAMESPACE}_TIME_ZONE',
-    'UTC'
-)
-
-BOT_NAME = NAMESPACE
+BOT_NAME = 'netkeiba'
 SPIDER_MODULES = ['netkeiba.spiders']
 NEWSPIDER_MODULE = 'netkeiba.spiders'
+
 ROBOTSTXT_OBEY = True
 
-COOKIES_ENABLED = getattr(
-    settings,
-    f'{NAMESPACE}_COOKIES_ENABLED',
-    False
-)
+COOKIES_ENABLED = getattr(settings, 'NETKEIBA_COOKIES_ENABLED', False)
 
-CRAWL_BFO = getattr(
-    settings,
-    f'{NAMESPACE}_CRAWL_BFO',
-    False
-)
+CRAWL_BFO = getattr(settings, 'NETKEIBA_CRAWL_BFO', False)
 
 if CRAWL_BFO:
     DEPTH_PRIORITY = 1
@@ -47,23 +25,11 @@ if CRAWL_BFO:
 
 AUTOTHROTTLE_ENABLED = True
 
-CONCURRENT_REQUESTS_PER_DOMAIN = getattr(
-    settings,
-    f'{NAMESPACE}_CONCURRENT_REQUESTS_PER_DOMAIN',
-    16
-)
+CONCURRENT_REQUESTS_PER_DOMAIN = getattr(settings, 'NETKEIBA_CONCURRENT_REQUESTS_PER_DOMAIN', 6)
 
-DOWNLOAD_DELAY = getattr(
-    settings,
-    f'{NAMESPACE}_DOWNLOAD_DELAY',
-    1.0
-)
+DOWNLOAD_DELAY = getattr(settings, 'NETKEIBA_DOWNLOAD_DELAY', 1.0)
 
-RETRY_TIMES = getattr(
-    settings,
-    f'{NAMESPACE}_RETRY_TIMES',
-    4
-)
+RETRY_TIMES = getattr(settings, 'NETKEIBA_RETRY_TIMES', 4)
 
 DOWNLOADER_MIDDLEWARES = {
     'netkeiba.middlewares.UserAgentMiddleware': 300,
