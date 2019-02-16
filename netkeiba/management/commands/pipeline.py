f590f340b102e53b26a725cef41c5e707cb7cae1
Hank Ehly <henry.ehly@gmail.com>
2019-02-16 12:09:05 +0900

    Update options in pipeline command

--- netkeiba/management/commands/pipeline.py
+++ netkeiba/management/commands/pipeline.py
@@ -15,33 +15,32 @@ class Command(BaseCommand):
     help = 'Execute the netkeiba training pipeline (scrape, import, backup)'
 
     def add_arguments(self, parser):
-        parser.add_argument('-s', '--shutdown', action='store_true', default=False,
+        parser.add_argument('--min-date', dest='min_date', type=date_string,
+                            help='Scrape all races that come on or after this date (fmt: YYYY-MM-DD)')
+        parser.add_argument('--max-date', dest='max_date', type=date_string,
+                            help='Scrape all races that come on or before this date (fmt: YYYY-MM-DD)')
+        parser.add_argument('--shutdown', action='store_true', default=False,
                             help='Execute shutdown command after pipeline completion')
-        parser.add_argument('-m', '--min-date', dest='min_date', type=date_string,
-                            help='Process all data from this date to present (fmt: YYYY-MM-DD)')
+        parser.add_argument('--backup', action='store_true', default=False,
+                            help='Make gzip copy of sqlite database after running scrape and import commands')
 
     def handle(self, *args, **options):
         started_at = datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
         logger.info(f'Started pipeline command at {started_at}')
 
-        if options['min_date']:
-            logger.info(f"user specified minimum date is {options['min_date']}")
-            min_date = options['min_date']
-        else:
-            logger.info('user specified minimum date does not exist (defaulting to now - 1 week)')
-            min_date = (started_at - timedelta(weeks=1)).strftime('%Y-%m-%d')
-
         scrapy_job_dirname = started_at.strftime('%Y-%m-%dT%H%M%S')
         logger.debug(f'Scrapy job dirname set to {scrapy_job_dirname}')
 
-        call_command('scrape', scrapy_job_dirname, min_date=min_date)
+        call_command('scrape', scrapy_job_dirname, min_date=options.get('min_date'), max_date=options.get('max_date'))
         call_command('import', scrapy_job_dirname)
-        call_command('backup')
+
+        if options.get('backup'):
+            call_command('backup')
 
         stopped_at = datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
         duration = (stopped_at - started_at).seconds
         logger.info(f'Finished pipeline command at {stopped_at} ({duration} seconds)')
 
-        if options['shutdown']:
+        if options.get('shutdown'):
             logger.info('Received shutdown option. Shutting down now.')
             call('sudo shutdown -h now', shell=True)
